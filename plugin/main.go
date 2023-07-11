145555a64f52e84203ef2d2c19076a2302de85b0
Ceyhun Onur <ceyhunonur54@gmail.com>
2023-07-11 22:20:39 +0300

    export Version and bump version (#15)

--- plugin/main.go
+++ plugin/main.go
@@ -16,9 +16,9 @@ import (
 	//_ "github.com/ava-labs/precompile-evm/{yourprecompilepkg}"
 )
 
-const precompileEVMVersion = "v0.0.0"
+const Version = "v0.1.1"
 
 func main() {
-	versionString := fmt.Sprintf("Precompile-EVM/%s Subnet-EVM/%s [AvalancheGo=%s, rpcchainvm=%d]", precompileEVMVersion, evm.Version, version.Current, version.RPCChainVMProtocol)
+	versionString := fmt.Sprintf("Precompile-EVM/%s Subnet-EVM/%s [AvalancheGo=%s, rpcchainvm=%d]", Version, evm.Version, version.Current, version.RPCChainVMProtocol)
 	runner.Run(versionString)
 }
