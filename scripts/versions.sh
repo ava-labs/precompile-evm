d75faa2c1931f98b01973565cbd4dc569ef34fcf
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-11-09 16:20:35 +0300

    replace avalanchego var name

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,5 +3,5 @@
 # Set up the versions to be used - populate ENV variables only if they are not already populated
 SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-'v0.5.9'}
 # Don't export them as they're used in the context of other calls
-AVALANCHEGO_VERSION=${AVALANCHE_VERSION:-'v1.10.15'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'v1.10.15'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
