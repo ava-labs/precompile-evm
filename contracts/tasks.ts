a8571653694807a89e8f9924f6b5d209bfa169d6
Ceyhun Onur <ceyhun.onur@avalabs.org>
2024-06-12 12:28:03 +0300

    Prepare release v0.2.3 (#70)
    * bump subnet-evm v0.6.5
    
    * npm update
    
    * bump subnet-evm npm pkg
    
    * bump subnet-evm & avalanchego
    
    * update mingo version
    
    * add release prepare
    
    * add typechain generate to build

--- contracts/tasks.ts
+++ contracts/tasks.ts
@@ -1,18 +1,17 @@
 import { task } from "hardhat/config"
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-import { BigNumber } from "ethers"
+
 
 task("accounts", "Prints the list of accounts", async (args, hre): Promise<void> => {
-  const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
-  accounts.forEach((account: SignerWithAddress): void => {
+  const accounts = await hre.ethers.getSigners()
+  accounts.forEach((account): void => {
     console.log(account.address)
   })
 })
 
 task("balances", "Prints the list of account balances", async (args, hre): Promise<void> => {
-  const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
+  const accounts = await hre.ethers.getSigners()
   for (const account of accounts) {
-    const balance: BigNumber = await hre.ethers.provider.getBalance(
+    const balance = await hre.ethers.provider.getBalance(
       account.address
     )
     console.log(`${account.address} has balance ${balance.toString()}`)
@@ -24,9 +23,7 @@ task("balance", "get the balance")
   .addParam("address", "the address you want to know balance of")
   .setAction(async (args, hre) => {
     const balance = await hre.ethers.provider.getBalance(args.address)
-    const balanceInCoin = hre.ethers.utils.formatEther(balance)
+    const balanceInCoin = hre.ethers.formatEther(balance)
     console.log(`balance: ${balanceInCoin} Coin`)
   })
-
-
 
