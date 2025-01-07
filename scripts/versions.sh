7261151e8541dfb493fe558f163f935295bf6518
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-01-07 21:42:30 +0300

    bump avago to 1.12.0

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,5 +3,5 @@
 # Set up the versions to be used - populate ENV variables only if they are not already populated
 SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-'v0.7.0'}
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'v1.12.1'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'v1.12.0'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
